6b8fb15bf365b4d616a9963be7070b358ed32fb6
Zendac101 <marklouie.camu06@gmail.com>
2026-08-29 16:09:29 +0800

    up url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registerUser', views.LogRes, name='registerUser'),
+    path('', views.LogRes, name='registerUser'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
