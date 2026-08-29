79ac7b0e2f81a28fd09f185d3a3879627f8ee59c
Zendac101 <marklouie.camu06@gmail.com>
2026-08-29 17:05:03 +0800

    accouints

--- aurae_monitoring/urls.py
+++ aurae_monitoring/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
 
     path('bossbaby/', admin.site.urls),
-    path('/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin_dashboard/', include('admin_dashboard.urls')),
 
 ]
