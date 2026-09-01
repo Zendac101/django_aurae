c44b2bc3f74797ef771e5b568496ee1e23f0e98c
Zendac101 <marklouie.camu06@gmail.com>
2026-09-01 21:12:39 +0800

    back to smtp

--- aurae_monitoring/settings.py
+++ aurae_monitoring/settings.py
@@ -185,7 +185,8 @@ ANYMAIL = {
 LOGIN_REDIRECT_URL = '/'
 if ENVIRONMENT == "production" or POSTGRES_LOCALLY == True:
 
-    EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+    # EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = env('EMAIL_ADDRESS')
